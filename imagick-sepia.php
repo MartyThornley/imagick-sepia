47e3d3127c2b816abe4ae7d3f43f2465adbc2e4b
Mike Schroder <mike.schroder@dreamhost.com>
2013-01-04 15:55:36 -0800

    Clarify comments; Spacing.

--- imagick-sepia.php
+++ imagick-sepia.php
@@ -9,13 +9,15 @@ Author URI: https://www.getsource.net
 */
 
 /**
- * Add Sepia Editor to beginning of search list.
- * Unnecessesary if specifically requesting sepia() method, but safer overall.
+ * Add Sepia Editor to beginning of editor search array.
+ *
+ * The new editor doesn't need to be at beginning if specifically requesting
+ * an editor with sepia() method, but it's safer overall.
  */
 function gs_add_imagick_sepia( $editors ) {
 	include_once 'editors/imagick-sepia.php';
-	array_unshift( $editors, 'GS_Imagick_Sepia_Editor' );
 
+	array_unshift( $editors, 'GS_Imagick_Sepia_Editor' );
 	return $editors;
 }
 add_filter( 'wp_image_editors', 'gs_add_imagick_sepia' );
