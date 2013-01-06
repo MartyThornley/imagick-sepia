4f3f4319e54e45c1155f6ace45c25bbaf1589ce9
Mike Schroder <mike.schroder@dreamhost.com>
2013-01-06 00:23:54 -0800

    Include based on class existing to avoid include/check performance issues

--- imagick-sepia.php
+++ imagick-sepia.php
@@ -15,7 +15,8 @@ Author URI: https://www.getsource.net
  * an editor with sepia() method, but it's safer overall.
  */
 function gs_add_imagick_sepia( $editors ) {
-	include_once 'editors/imagick-sepia.php';
+	if( ! class_exists( 'GS_Imagick_Sepia_Editor' ) )
+		include( plugin_dir_path( __FILE__ ) . 'editors/imagick-sepia.php' );
 
 	array_unshift( $editors, 'GS_Imagick_Sepia_Editor' );
 	return $editors;
